d444529ea0e4050cd66a5c2003ec42ff52029d72
Pavel Tikhomirov <118762514+txmrv@users.noreply.github.com>
2022-12-03 02:11:38 +0300

    feat add gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import play from '../src/index.js';
+import game from '../src/games/gcd.js';
+
+play(game);
---
+++ src/games/gcd.js
@@ -0,0 +1,36 @@
+import getRandomInt from '../utils.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getGCD = (a, b) => {
+  const greatest = a < b ? b : a;
+  const smallest = a < b ? a : b;
+
+  for (let i = 1; i <= (smallest / 2); i += 1) {
+    const remainder = smallest % i;
+
+    if (remainder === 0) {
+      const divisor = smallest / i;
+      const isCommonDivisor = greatest % divisor === 0;
+
+      if (isCommonDivisor) {
+        return divisor;
+      }
+    }
+  }
+
+  return 1;
+};
+
+const getChallenge = () => {
+  const maxNumber = 100;
+  const a = getRandomInt(maxNumber);
+  const b = getRandomInt(maxNumber);
+
+  const question = `${a} ${b}`;
+  const answer = `${getGCD(a, b)}`;
+
+  return [question, answer];
+};
+
+export default [description, getChallenge];
